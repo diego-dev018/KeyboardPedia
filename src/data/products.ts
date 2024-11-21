758c272c666b46d612af5a3d64a53fe19c79729f
Diego Ronaldo Sanchez <diegosanchez.dev018@gmail.com>
2024-11-20 23:13:16 -0600

    FirstCommit

---
+++ src/data/products.ts
@@ -0,0 +1,92 @@
+export const products = [
+    {
+        title: 'Product 1',
+        description: 'Description 1',
+        id: '1',
+        stores: [
+            {
+                name: 'Store 1',
+                href: 'https://store1.com'
+            },
+        ],
+        bg_card_src: '',
+        bg_img_src: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.metro951.com%2Fwp-content%2Fuploads%2F2018%2F03%2F1519910473_492871_1519912094_album_normal.jpg&f=1&nofb=1&ipt=d17a55d2379ac4ec5f3287c766a075bd99a7d2afee529f1cf0e5104e3f81f32b&ipo=images',
+        tags: [
+            'tag1',
+            'tag2'
+        ],
+        type: 'type1'
+    },
+    {
+        title: 'Product 2',
+        description: 'Description 2',
+        id: '2',
+        stores: [
+            {
+                name: 'Store 2',
+                href: 'https://store2.com'
+            },
+        ],
+        bg_card_src: '',
+        bg_img_src: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.metro951.com%2Fwp-content%2Fuploads%2F2018%2F03%2F1519910473_492871_1519912094_album_normal.jpg&f=1&nofb=1&ipt=d17a55d2379ac4ec5f3287c766a075bd99a7d2afee529f1cf0e5104e3f81f32b&ipo=images',
+        tags: [
+            'tag1',
+            'tag2'
+        ],
+        type: 'type1'
+    },
+    {
+        title: 'Product 3',
+        description: 'Description 3',
+        id: '3',
+        stores: [
+            {
+                name: 'Store 3',
+                href: 'https://store3.com'
+            },
+        ],
+        bg_card_src: '',
+        bg_img_src: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.metro951.com%2Fwp-content%2Fuploads%2F2018%2F03%2F1519910473_492871_1519912094_album_normal.jpg&f=1&nofb=1&ipt=d17a55d2379ac4ec5f3287c766a075bd99a7d2afee529f1cf0e5104e3f81f32b&ipo=images',
+        tags: [
+            'tag1',
+            'tag2'
+        ],
+        type: 'type1'
+    },
+    {
+        title: 'Product 4',
+        description: 'Description 4',
+        id: '4',
+        stores: [
+            {
+                name: 'Store 4',
+                href: 'https://store4.com'
+            },
+        ],
+        bg_card_src: '',
+        bg_img_src: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.metro951.com%2Fwp-content%2Fuploads%2F2018%2F03%2F1519910473_492871_1519912094_album_normal.jpg&f=1&nofb=1&ipt=d17a55d2379ac4ec5f3287c766a075bd99a7d2afee529f1cf0e5104e3f81f32b&ipo=images',
+        tags: [
+            'tag1',
+            'tag2'
+        ],
+        type: 'type1'
+    },
+    {
+        title: 'Product 5',
+        description: 'Description 5',
+        id: '5',
+        stores: [
+            {
+                name: 'Store 5',
+                href: 'https://store5.com'
+            },
+        ],
+        bg_card_src: '',
+        bg_img_src: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.metro951.com%2Fwp-content%2Fuploads%2F2018%2F03%2F1519910473_492871_1519912094_album_normal.jpg&f=1&nofb=1&ipt=d17a55d2379ac4ec5f3287c766a075bd99a7d2afee529f1cf0e5104e3f81f32b&ipo=images',
+        tags: [
+            'tag1',
+            'tag2'
+        ],
+        type: 'type1'
+    },
+];
