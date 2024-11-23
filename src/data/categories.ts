9d00c287852755a5c7cd6396f8c7c777cb4f257b
Diego Ronaldo Sanchez <diegosanchez.dev018@gmail.com>
2024-11-22 23:21:59 -0600

    Header-Refactor&Categories:Index-Create&RoutesRelatives-ToAbsolutes

---
+++ src/data/categories.ts
@@ -0,0 +1,13 @@
+export const categories = [
+    {
+        id: 'keyboards',
+        title: 'KeyBoards',
+        description: '',
+        bg_img_src: '',
+        tags: [
+            'tag1',
+            'tag2'
+        ],
+        image: '',
+    }
+]
