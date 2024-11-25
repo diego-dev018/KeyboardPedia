6d02ae65084caba0b72495f294467134e1b86ce3
Diego Ronaldo Sanchez <diegosanchez.dev018@gmail.com>
2024-11-24 23:18:48 -0600

    Index-Update&Categories-Del

---
+++ src/data/me.ts
@@ -0,0 +1,17 @@
+export const me = {
+    name: 'Diego Sanchez',
+    socialNetworks: [
+        {
+            name: 'LinkedIn',
+            url: 'https://www.linkedin.com/in/diego-ronaldo-sanchez-pichardo-803a1a317/'
+        },
+        {
+            name: 'GitHub',
+            url: 'https://github.com/diego-dev018'
+        },
+        {
+            name: 'YouTube',
+            url: 'https://www.youtube.com/@diegodev01'
+        }
+    ]
+}
