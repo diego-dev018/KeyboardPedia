498ab1fe57cdd484b1efbe4bb505a0529de27dc6
Diego <diegosanchez.dev018@gmail.com>
2024-11-25 14:18:24 -0600

    Enhance UI components: update styles for header, titles, buttons, and add keyboard gallery

---
+++ src/data/keyboardsToGalery.ts
@@ -0,0 +1,9 @@
+const directory = 'keyboardsToGalery';
+
+export const keyboardsToGalery = [
+    `${directory}/TecladoCustom(1).webp`,
+    `${directory}/TecladoCustom(2).webp`,
+    `${directory}/TecladoCustom(3).webp`,
+    `${directory}/TecladoCustom(4).webp`,
+    `${directory}/TecladoCustom(5).webp`,
+];
