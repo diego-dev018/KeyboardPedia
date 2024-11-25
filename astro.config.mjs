93d820118bfd99e3326ae6e82000e9fc2e069065
Diego <diegosanchez.dev018@gmail.com>
2024-11-25 09:01:14 -0600

    InstallVercel-Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: vercel(),
   integrations: [
     tailwind(),
     mdx()
