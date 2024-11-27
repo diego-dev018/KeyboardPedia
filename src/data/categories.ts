038a0117bb72445f054dd51300061253710a36eb
Diego Ronaldo Sanchez <diegosanchez.dev018@gmail.com>
2024-11-27 13:17:51 -0600

    Refactor categories data into a separate module for improved maintainability

---
+++ src/data/categories.ts
@@ -0,0 +1,47 @@
+export const categoriesForPage = [
+    {
+        name: "Keyboards",
+        bg_img: "",
+        href: "/keyboards",
+    },
+    {
+        name: "Switches",
+        bg_img: "",
+        href: "/switches",
+    },
+    {
+        name: "Keycaps",
+        bg_img: "",
+        href: "/keycaps",
+    },
+    {
+        name: "Pcb",
+        bg_img: "",
+        href: "/pcb",
+    },
+    {
+        name: "Plate",
+        bg_img: "",
+        href: "/plate",
+    },
+    {
+        name: "Case",
+        bg_img: "",
+        href: "/case",
+    },
+    {
+        name: "Stabilizer",
+        bg_img: "",
+        href: "/stabilizer",
+    },
+    {
+        name: "Cables",
+        bg_img: "",
+        href: "/cables",
+    },
+    {
+        name: "Shock Absorber",
+        bg_img: "",
+        href: "/shockAbsorber",
+    },
+];
