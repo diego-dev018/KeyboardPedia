ca11aeb87d3f866ef65ed0da0a01c68412a58ca2
Diego <diegosanchez.dev018@gmail.com>
2024-12-06 10:04:40 -0600

    Add product example data and create product detail page

---
+++ src/data/productExample.ts
@@ -0,0 +1,57 @@
+export const myExample = [
+    {
+        id: 'nate-gentile-cycle-n8-founders-edition',
+        title: 'Teclado Nate Gentile Cycle N8 - Founders Edition',
+        description: [
+            {
+                title: 'Pre-venta: Entrega estimada en Marzo de 2025',
+                content: 'Este exclusivo teclado mecánico, fabricado en aluminio 6063-T6 y acero inoxidable, es una edición limitada de solo 500 unidades, creada para conmemorar el lanzamiento de la marca "N8" de Nate Gentile. Una pieza única que combina elegancia y funcionalidad, lista para usar desde el momento en que la sacas de la caja.'
+            },
+            {
+                title: 'Diseño TKL para Espacios Modernos',
+                content: 'El Teclado Cycle N8 es TKL (Ten-Key-Less), lo que significa que no tiene la zona numérica del teclado. Esto está pensado para maximizar el espacio en tu escritorio y tener así más rango de movimiento con el ratón, algo especialmente beneficioso en videojuegos. Su diseño compacto y minimalista lo hace perfecto para setups modernos, optimizando el flujo y la estética de tu espacio de trabajo.'
+            },
+            {
+                title: 'Montaje Gasket para una Experiencia Única',
+                content: 'Montado con el sistema "gasket", ofrece una experiencia de tecleo suave, consistente y con una acústica mejorada. Cada pulsación se siente precisa y confortable, transformando el acto de escribir o jugar en una experiencia premium.'
+            },
+            {
+                title: 'Switches Exclusivos Vertex V1.2 x Nate Gentile',
+                content: 'Equipado con los exclusivos interruptores Vertex V1.2 Nate Edition, diseñados en colaboración con Vertex. Estos switches de edición limitada, fabricados en nylon y POM de estilo "long pole", son lineales, ultrasuaves y rápidos en respuesta, ofreciendo un sonido sutil y agradable que complementa la experiencia.'
+            },
+            {
+                title: 'Teclas en PBT y Leyendas en Español ISO',
+                content: 'Las teclas están hechas de PBT, un material duradero y resistente al desgaste. Con leyendas en español ISO de España, incluyendo la tecla "ñ", este teclado ofrece una experiencia completamente optimizada para el público hispanohablante.'
+            },
+            {
+                title: 'Versatilidad y Estilo en Cada Pulsación',
+                content: 'Ideal para gaming, escritura y programación, el Teclado N8 Cycle – Founder’s Edition añade un toque de distinción y minimalismo a cualquier setup. Es mucho más que un teclado: es una declaración de estilo y calidad que eleva tu escritorio al siguiente nivel.'
+            },
+        ],
+        short_description: 'Este es un producto de ejemplo',
+        images: [
+            'https://store.nategentile.com/cdn/shop/files/teclado1.jpg',
+            'https://store.nategentile.com/cdn/shop/files/teclado2.jpg',
+            'https://store.nategentile.com/cdn/shop/files/teclado3.jpg',
+            'https://store.nategentile.com/cdn/shop/files/teclado4.jpg',
+            'https://store.nategentile.com/cdn/shop/files/teclado5.jpg',
+        ],
+        by: 'Nate Gentile',
+        price: {
+            mxn: 7627.00,
+            usd: 381.35,
+            eur: 320.00
+        },
+        page_seller: 'https://store.nategentile.com/products/teclado-nate-gentile-cycle-n8-founder-s-edition',
+        stock: 0,
+        tags: [
+            'teclado',
+            'nate-gentile',
+            'cycle-n8',
+            'founders-edition',
+            'by-youtube-creator',
+            'by-streamer',
+            'by-influencer',
+        ]
+    }
+]
